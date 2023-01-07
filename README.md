98bd8802fdc849a8d23680840b5c7691e66dcd93
lronhubbard-canwest <79243288+lronhubbard-canwest@users.noreply.github.com>
2023-01-06 16:48:34 -0800

    v1.1 released
    Bug fixed, updated, fixed example, and better comments.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # esdt-bulksend-by-csv
-This BASH script reads a CSV file to send corresponding ESDT tokens, using erdpy.  Customized for STEAKS token.  
+This BASH script reads a CSV file to send corresponding ESDT tokens, using erdpy.  Customized for steak4all's fine STEAKS token.  
 
 Version 1.0 released to Elrond community Feb 16th 2022 by steak4all, feel free to use in any way useful.  
 GNU bash 5.0.17(1) used (default of ubuntu 20.04) 
@@ -8,7 +8,7 @@ Have many Elrond Digital Standard Tokens to send, but have no way to send to man
 
 How to use this script? 
 
-Prerequisite:  install erdpy https://docs.elrond.com/sdk-and-tools/erdpy/installing-erdpy/#docsNav
+Prerequisite: Linux/BASH.  And then install and configure erdpy https://docs.elrond.com/sdk-and-tools/erdpy/installing-erdpy/#docsNav
 
 The script sends tokens via erdpy. You'll need to setup your wallet for sending and use, as described in the link above. 
 
@@ -17,11 +17,17 @@ nomenclature, which can be changed if you like. Also note to change the chain yo
 
 Finally, prepare your CSV file, using the sample as a guide. Once you have your CSV file created, be sure to name it the same or change the corresponding file name in the script.
 
+One really super-duper important thing we found out after a lot of hair pulling was that for your CSV file, unless you made it in Linux, want to use the program DOS2UNIX (available in Ubuntu) to
+first process the CSV file. Even if you d/led from a cloud source like Google Drive, or if you have any hex calculations coming up, use DOS2UNIX on your CSV like this: 
+dos2unix mysample.csv 
+Many bothans died for this information. 
+
 Note: 
 You'll need to 
 chmod a+x esdt-csv-send.sh
 before it'll run.
 
 Change log:  
-June 3rd v1.01 update, fixed some small things, and added more description.  NOTE that a bug was found and still exists.  The script only seems to work up to 99 entries in the CSV file.  After that, an error in the hex field conversion seems to happen. 
+Jan 6th 2023 v1.1 big clean up and update of the code. Found the dreaded HEX bug and killed it, see note on DOS2UNIX above. Updated example csv. 
+June 3rd 2022  v1.01 update, fixed some small things, and added more description.  NOTE that a bug was found and still exists.  The script only seems to work up to 99 entries in the CSV file.  After that, an error in the hex field conversion seems to happen. 
 Until this is fixed,break your send lists down to file increments of up to a max of 99 TXs per CSV.
--- esdt-csv-send.sh
+++ esdt-csv-send.sh
@@ -7,9 +7,11 @@ EXTRADECIMAL="000000"
 #mainnet chainID: 1
 chainID="1"
 PROXY="https://gateway.elrond.com"
+#the above two lines confirm this is on mainnet
 MYWALLET="erd1xau0xlwlzldje349cxgngnaxym6mmn2pertqkk6ev7dmrlg9urysxjdlyv"
-PEM_FILE="./steaksp.pem"
-#text color
+#obv you'll want to change this above to your own my wallet address. 
+PEM_FILE="./privatewalletkey.pem"
+#text colors, for no particular reason are the next 4 lines
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 LBLUE='\033[1;34m'
@@ -25,51 +27,68 @@ printf "...${RED}Starting...${NC}\n"
 #echo -e "Starting.."
 while IFS="," read -r send_address_c1 steaks_c2
 do
+  extradec="000000"
   #get nonce
   NONCE=$(erdpy account get --nonce --address="$MYWALLET" --proxy="$PROXY")
+  rm hexfordatafield.txt
   extra0orspace=""
   #send address display
   echo -e " \n"
   echo -e "${LBLUE}Send address: ${YELLOW} $send_address_c1"
-  echo -e "${LBLUE}Nonce: ${YELLOW} $NONCE \n"
-  echo -e "${LBLUE}Quantity of steaks: ${YELLOW} $steaks_c2 \n"
+  echo -e " \n"
+  echo -e "${LBLUE}Nonce: ${YELLOW} $NONCE"
+  echo -e " \n"
+  echo -e "${LBLUE}Quantity of steaks: ${NC} $steaks_c2"
+  #adjustedsteaks=$(echo $steaks_c2|tr -d '\n\r')
   #turn readable csv steaks value into proper value
-  adjustedsteaks_c2="$steaks_c2$EXTRADECIMAL"
-  #adjustedsteaks_c2=$steaks_c2
-  #adjustedsteaks_c2+=$EXTRADECIMAL
-  echo -e "${LBLUE}Adjusted no. of steaks:${YELLOW} $adjustedsteaks_c2 ${NC}\n" 
-#hexfordatafield="printf '$extra0orspace%x\n' $steaks_c2$EXTRADECIMAL | tee hexfordatafield.txt"
-hexfordatafield="printf '$extra0orspace%x\n' $adjustedsteaks_c2 | tee hexfordatafield.txt"
-echo -e "${LBLUE}Converted to hex:${YELLOW}"
-eval $hexfordatafield
-hexfordatafield=$( cat hexfordatafield.txt )
-#make data field
-stringlength=${#hexfordatafield}
-echo -e "${LBLUE}Adjusted esdt amount hex-field DATA string length: ${YELLOW} $stringlength ${LBLUE}"
-#the following checks to see if the hex field character length is odd or even. If it is odd, a "0" need to be added to the data field for it to be read properly 
-if [ $((stringlength%2)) -eq 0 ]
-then
-  echo -e "${LBLUE}Hex field length is even."
-else
-  echo -e "${LBLUE}Hex field length is odd. Adding 0 to start of hex for DATA field..."
-  extra0orspace="0" 
-fi
-datafield="ESDTTransfer@535445414b532d616262396631@$extra0orspace$hexfordatafield"
-#important!!! this line above is set to STEAKS token currently. You must change @535445414b532d616262396631 to the token you are using
-#which you can see by sending a normal ESDT token from your wallet and noting the identifier field your token uses 
-echo -e "${LBLUE}Data Field:${YELLOW}\n"
-echo -e "$datafield${LBLUE}\n"
-#refernce:  erdpy --verbose tx new --send --outfile="./sent-tx-$NONCE.json" --pem=$PEM_FILE --nonce=$NONCE --receiver=$1 --value="$2$DENOMINATION" --gas-limit=50000 --proxy=$PROXY
-# my version:
-echo -e "${LBLUE}Preparing tx as follows:${YELLOW}\n"
-echo "erdpy --verbose tx new --send --outfile="sent-tx-$NONCE.json" --pem=$PEM_FILE --nonce=$NONCE --receiver=$send_address_c1"" --value=0 --gas-limit 500000 --chain=$chainID"" --proxy=$PROXY --data=$datafield"
-sendtx="erdpy --verbose tx new --send --outfile="sent-tx-$NONCE.json" --pem=$PEM_FILE --nonce=$NONCE --receiver=$send_address_c1"" --value=0 --gas-limit 500000 --chain=$chainID"" --proxy=$PROXY --data=$datafield"
-#note the following sleeps are needed because with out them, the sending will happen to fast -- which throws off the nonce, as nonce only chages after a TX has had time to be processed 
-sleep 6
-eval $sendtx
-#echo "Transaction sent with nonce $NONCE and backed up to bon-mission-tx-$NONCE.json."
-#reference DATA field for steaks4all esdt transfer:      ESDTTransfer@535445414b532d616262396631@e4e1c0      ESDTTransfer@535445414b532d616262396631@e4e1c0
-echo -e "${LBLUE}---------Transaction sent!"
-echo -e "Proceeding to next transaction----"
-sleep 6
-done < <(tail -n +2 thisismysendlist.csv)
+  #prepsteaks="${steaks_c2}"
+  #echo -n "Value of prep steaks: $prepsteaks" 
+  #adjustedsteaks=$(echo $steaks_c2|tr -d '\n')
+  #adjustedsteaks=$steaks_c2$EXTRADECIMAL
+  echo -e "${LBLUE} \n"
+  echo -n "Adjusted no. of steaks:"
+  adjustedsteaks=$steaks_c2$EXTRADECIMAL
+  echo "Is:$adjustedsteaks"
+  #prepsteaks="$adjustedsteaks_c2"
+  #echo -n "Second prep adjustment of steaks: "
+  #echo -n " $prepsteaks"
+  echo -e " \n"
+  echo -e "${LBLUE}Converted to hex:${YELLOW}\n"
+  #printf "%x\n", $adjustedsteaks | tee hexfordatafield.txt
+  printf "%x" $adjustedsteaks | tee hexfordatafield.txt
+  #above line does a hex conversion and saves the output to the txt file
+  hexfordatafield=$(cat hexfordatafield.txt)
+  #make data field of hex 
+  stringlength=${#hexfordatafield}
+  echo -e " \n"
+  echo -e "${LBLUE}Adjusted esdt amount hex-field DATA string length: ${YELLOW} $stringlength ${LBLUE}"
+  #the following checks to see if the hex field character length is odd or even. If it is odd, a "0" need to be added to the data field for it to be read properl
+  if [ $((stringlength%2)) -eq 0 ]
+   then
+   echo -e "${LBLUE}Hex field length is even."
+   else
+   echo -e "${LBLUE}Hex field length is odd. Adding 0 to start of hex for DATA field..."
+   extra0orspace="0"
+  fi
+ datafield="ESDTTransfer@535445414b532d616262396631@$extra0orspace$hexfordatafield"
+ #important!!! this line above is set to STEAKS token currently. You must change @535445414b532d616262396631 to the token you are using. You can get this hex ID 
+ #from checking the original smart contract that created the token, or use a tool to convert.
+ #or also you can see your hex ID by sending the ESDT token from your wallet and noting the identifier field your token uses
+ echo -e "${LBLUE}Data Field:${YELLOW}\n"
+ echo -e "$datafield${LBLUE}\n"
+ #refernce:  erdpy --verbose tx new --send --outfile="./sent-tx-$NONCE.json" --pem=$PEM_FILE --nonce=$NONCE --receiver=$1 --value="$2$DENOMINATION" --gas-limit=50000 --proxy=$PROXY
+ # my version:
+ echo -e "${LBLUE}Preparing tx as follows:${YELLOW}\n"
+ echo "erdpy --verbose tx new --send --outfile="sent-tx-$NONCE.json" --pem=$PEM_FILE --nonce=$NONCE --receiver=$send_address_c1"" --value=0 --gas-limit 500000 --chain=$chainID"" --proxy=$PROXY --data=$datafield"
+ sendtx="erdpy --verbose tx new --send --outfile="sent-tx-$NONCE.json" --pem=$PEM_FILE --nonce=$NONCE --receiver=$send_address_c1"" --value=0 --gas-limit 500000 --chain=$chainID"" --proxy=$PROXY --data=$datafield"
+ #note the following sleeps are needed because with out them, the sending will happen to fast -- which throws off the nonce, as nonce only chages after a TX has had time to be processed 
+ sleep 9
+ eval $sendtx
+ echo -n "Transaction sent with nonce $NONCE and backed up to bon-mission-tx-$NONCE.json. \n"
+ #reference DATA field for steaks4all esdt transfer:      ESDTTransfer@535445414b532d616262396631@e4e1c0      ESDTTransfer@535445414b532d616262396631@e4e1c0
+ echo -e "${LBLUE}---------Transaction sent!"
+ echo -e "${RED} Proceeding to next transaction----${NC}"
+ sleep 6
+done < <(tail -n +1 samplesend.csv)
+#change the file name above as needed. 
+#and remember to eat vegetables as well, sometimes, steaks need side dishes 
